f6e5cb51ab270f4cf91d520707664074d914ece0
Frank Yang <codebear4@gmail.com>
2016-08-03 16:34:05 +0800

    Fix parameter for `clock_gettime` function.
    Signed-off-by: Frank Yang <codebear4@gmail.com>

--- progress.c
+++ progress.c
@@ -34,7 +34,7 @@ timestamp(struct timespec *ts) {
   ts->tv_sec = mts.tv_sec;
   ts->tv_nsec = mts.tv_nsec;
 #else
-  clock_gettime(CLOCK_REALTIME, &ts);
+  clock_gettime(CLOCK_REALTIME, ts);
 #endif
 }
 
